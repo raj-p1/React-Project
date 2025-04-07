b779688850355c74eab669e25d5c00bca8ca432d
Raj Patel <rp94@njit.edu>
2025-04-07 16:07:28 +0530

    Completed Counters, Tailwind CSS, Props

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react';
+import './App.css';
+import Counter from './components/counter';
+import CounterIn from './components/counterIn';
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Counter />
+      <CounterIn />
+    </>
+  );
+}
+
+export default App;
